5f23c68a81cc7141c9f58e51e33df81b8a0e2249
CarolinaVitale <vitale.carola@gmail.com>
2024-08-19 16:39:24 -0400

    burger-beer

--- src/components/ImageAnimation.js
+++ src/components/ImageAnimation.js
@@ -2,38 +2,29 @@ import React from 'react';
 import { Button } from './Button';
 import { useInView } from 'react-intersection-observer';
 import { motion } from 'framer-motion';
-import '../components/Animation.css';
+import '../components/ImageAnimation.css';
 
-const ChefAnimation = ({ image, text, paragraph, paragraph2, button }) => {
+const ImageAnimation = ({ image, tittle, paragraph, paragraph2, button }) => {
     const [AnimationRef, AnimationInView] = useInView({ threshold: 0.5 });
     const [AnimationRef2, AnimationInView2] = useInView({ threshold: 0.5 });
     const [AnimationRef3, AnimationInView3] = useInView({ threshold: 0.5 });
-    const [AnimationRef4, AnimationInView4] = useInView({ threshold: 0.5 });
 
 
     return (
-        <div className="scroll-animation-container">
 
-            <motion.div
-                className="image-container"
-                ref={AnimationRef4}
-                initial={{ opacity: 0, x: 50 }}
-                animate={{ opacity: AnimationInView4 ? 1 : 0, x: AnimationInView4 ? 0 : 50 }}
-                transition={{ duration: 0.5 }}
-            >
-                <img src={image} alt="placeholder" />
-            </motion.div>
+        <div className="animation-container">
+            <img className='image-animation' src={image} alt="placeholder" />
 
-            <div className="text-container">
+            <div className="text-container-animation">
                 <motion.div
                     ref={AnimationRef}
                     initial={{ opacity: 0, y: 50 }}
                     animate={{ opacity: AnimationInView ? 1 : 0, y: AnimationInView ? 0 : 50 }}
                     transition={{ duration: 0.5 }}
                 >
-                    <div className='text'>
+                    <div className='tittle-animation'>
                         <br />
-                        <h1>{text}</h1>
+                        <h1>{tittle}</h1>
                         <br />
                     </div>
                 </motion.div>
@@ -44,7 +35,7 @@ const ChefAnimation = ({ image, text, paragraph, paragraph2, button }) => {
                     animate={{ opacity: AnimationInView2 ? 1 : 0, y: AnimationInView2 ? 0 : -50 }}
                     transition={{ duration: 0.5 }}
                 >
-                    <div className='text'>
+                    <div className='paragraph'>
                         <br />
                         <p>{paragraph}</p>
                         <p>{paragraph2}</p>
@@ -59,20 +50,19 @@ const ChefAnimation = ({ image, text, paragraph, paragraph2, button }) => {
                     animate={{ opacity: AnimationInView3 ? 1 : 0, y: AnimationInView3 ? 0 : 50 }}
                     transition={{ duration: 0.5 }}
                 >
-                    <div className='text'>
+                    <div className='tittle'>
                         <Button
-                            where='/about'
+                            where='/menu'
                             className='btns'
-                            buttonStyle='btn--outline'
+                            buttonStyle='btn--primary'
                             buttonSize='btn--large'
                         >{button}</Button>
                     </div>
                 </motion.div>
             </div>
-
-            
         </div>
+
     );
 };
 
-export default ChefAnimation;
+export default ImageAnimation;
